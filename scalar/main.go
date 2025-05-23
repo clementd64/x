e777e58e2f521569b92a5945d2ca925eb38c6358
Clément Dubreuil <clement@dubreuil.dev>
2025-05-23 22:39:28 +0200

    scalar inject authentication

--- scalar/main.go
+++ scalar/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -10,17 +11,19 @@ import (
 )
 
 //go:embed scalar.html
-var index []byte
+var index string
 
 func main() {
 	addr := flag.String("addr", ":8080", "address to listen on")
 	openapi := flag.String("openapi", "schema.json", "API definition file")
 	gateway := flag.String("proxy", "", "Proxy address")
+	auth := flag.String("auth", "", "Prefill authentication")
 	flag.Parse()
 
+	t := template.Must(template.New("index").Parse(string(index)))
 	http.HandleFunc("/{$}", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
-		w.Write(index)
+		t.Execute(w, template.JS(*auth))
 	})
 
 	http.HandleFunc("/openapi.json", func(w http.ResponseWriter, r *http.Request) {
